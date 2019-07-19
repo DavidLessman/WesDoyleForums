af2e5690975317b51c725e0ac3968908a2972697
David J Lessman <35240546+TiberiusDarkTigger@users.noreply.github.com>
2019-07-19 16:37:02 -0500

    Add post service

--- WesDoyleForums/Controllers/ForumController.cs
+++ WesDoyleForums/Controllers/ForumController.cs
@@ -1,7 +1,10 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WesForum.Data;
+using WesForum.Data.Models;
 using WesForum.Models.Forum;
+using WesForum.Models.Post;
 
 namespace WesForum.Controllers
 {
@@ -35,9 +38,43 @@ namespace WesForum.Controllers
         public IActionResult Topic(int id)
         {
             var forum = _forumService.GetById(id);
-            var posts = _postService.GetFilteredPosts(id);
+            var posts = forum.Posts;
 
-            var postListings = 
+            var postListings = posts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                AuthorId = post.User.Id,
+                AuthorRating = post.User.Rating,
+                Title = post.Title,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = post.Replies.Count(),
+                Forum = BuildForumListing(post)
+            });
+
+            var model = new ForumTopicModel
+            {
+                Posts = postListings,
+                Forum = BuildForumListing(forum)
+            };
+
+            return View(model);
+        }
+
+        private ForumListingModel BuildForumListing(Post post)
+        {
+            var forum = post.Forum;
+            return BuildForumListing(forum);
+        }
+
+        private ForumListingModel BuildForumListing(Forum forum)
+        {
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                Name = forum.Title,
+                Description = forum.Description,
+                ImageUrl = forum.ImageUrl
+            };
         }
     }
 }
---
+++ WesDoyleForums/Models/Forum/ForumTopicModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WesForum.Models.Post;
+
+namespace WesForum.Models.Forum
+{
+    public class ForumTopicModel
+    {
+        public ForumListingModel Forum { get; set; }
+        public IEnumerable<PostListingModel> Posts { get; set; }
+    }
+}
