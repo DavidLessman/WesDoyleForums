ab13eef2ab11e53f8be16e341a19f2d6423d2915
David J Lessman <35240546+TiberiusDarkTigger@users.noreply.github.com>
2019-07-20 12:38:37 -0500

    When posting new Post... system saying that reply is Null and not redirecting properly

--- WesDoyleForums/Controllers/PostController.cs
+++ WesDoyleForums/Controllers/PostController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using WesForum.Data;
 using WesForum.Data.Models;
@@ -12,10 +14,15 @@ namespace WesForum.Controllers
     public class PostController : Controller
     {
         private readonly IPost _postService;
+        private readonly IForum _forumService;
 
-        public PostController(IPost postService)
+        private static UserManager<ApplicationUser> _userManager;
+
+        public PostController(IPost postService, IForum forumService, UserManager<ApplicationUser> userManager)
         {
             _postService = postService;
+            _forumService = forumService;
+            _userManager = userManager;
         }
 
         public IActionResult Index(int id)
@@ -37,6 +44,48 @@ namespace WesForum.Controllers
             };
 
             return View(model);
+        }
+
+        public IActionResult Create(int id)
+        {
+            //Note id is forum.Id
+            var forum = _forumService.GetById(id);
+            var model = new NewPostModel
+            {
+                ForumName = forum.Title,
+                ForumId = forum.Id,
+                ForumImageUrl = forum.ImageUrl,
+                AuthorName = User.Identity.Name
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPost(NewPostModel model)
+        {
+            var userId = _userManager.GetUserId(User);
+            var user = _userManager.FindByIdAsync(userId).Result;
+            var post = BuildPost(model, user);
+
+            _postService.Add(post).Wait();
+
+            // TODO: Implement User Rating Management
+
+            return RedirectToAction("Index", "Post", new { post = post.Id });
+        }
+
+        private Post BuildPost(NewPostModel model, ApplicationUser user)
+        {
+            var forum = _forumService.GetById(model.ForumId);
+
+            return new Post
+            {
+                Title = model.Title,
+                Content = model.Content,
+                Created = DateTime.Now,
+                User = user,
+                Forum = forum
+            };
         }
 
         private IEnumerable<PostReplyModel> BuildPostReplies(IEnumerable<PostReply> replies)
